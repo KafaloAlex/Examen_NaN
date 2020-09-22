18a86ddc6e4fdc1fa609ce471c3f6774db536037
Alex <ckalexis20@gmail.com>
2020-09-22 12:16:22 +0000

    Intégration du template

---
+++ Andrea/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Andrea.Startup))]
+namespace Andrea
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
